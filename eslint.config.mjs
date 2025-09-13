06905d337aac2f1faff7d8c9ac13d525aa778d85
Edard <edardmichaelu@gmail.com>
2025-09-13 13:15:14 -0500

    Ignore binary files for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-    // Reglas base de JS
-    js.configs.recommended,
-
-    // Reglas de TypeScript
-    ...tseslint.configs.recommended,
-
-    // Configuración para React
     {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-        plugins: {
-            react: pluginReact,
-        },
+        files: ["**/*.{js,mjs,cjs,jsx}"], // solo JS y JSX
+        ignores: ["test.js", "node_modules/**"], // ignorar test.js y node_modules
+        plugins: { react: pluginReact },
         settings: {
-            react: {
-                version: "detect", // 👈 Detecta automáticamente la versión de React
-            },
+            react: { version: "detect" },
         },
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-        },
-        rules: {
-            // Puedes agregar reglas personalizadas aquí si lo deseas
-        },
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+        rules: {},
     },
+    js.configs.recommended,
 ]);
